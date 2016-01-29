b1253b9bc1fa933e92f2b890c03b94b3cac851aa
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-29 11:42:54 -0800

    add price prototype and refactor 'toppings' as an array for it to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 function Pizza (size,topping1,topping2) {
   this.size = size;
-  this.topping1 = topping1;
-  this.topping2 = topping2;
+  this.toppings = [topping1,topping2]
+  this.toppings = this.toppings.filter(function(n){ return n != undefined });
+}
+
+Pizza.prototype.price = function() {
+  var basePrice = 7;
+  var toppingPrice = 2;
+
+
+
+  var pizzaPrice = basePrice + toppingPrice * this.toppings.length;
+
+  return pizzaPrice;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,25 @@
 describe("Pizza Constructor", function() {
   var testPizza = new Pizza (2,"cheese","pepperoni");
   it("has two toppings the user can select", function() {
-    expect(testPizza.topping1).to.equal("cheese");
-    expect(testPizza.topping2).to.equal("pepperoni");
+    expect(testPizza.toppings[0]).to.equal("cheese");
+    expect(testPizza.toppings[1]).to.equal("pepperoni");
   });
   it("has a size the user can select", function() {
     expect(testPizza.size).to.equal(2);
   })
+});
+
+describe("Pizza Price Prototype", function() {
+  it("has no toppings", function() {
+    var testPizza = new Pizza (2);
+    expect(testPizza.price()).to.equal(7);
+  });
+  it("has one topping", function() {
+    var testPizza = new Pizza (2,"cheese");
+    expect(testPizza.price()).to.equal(9);
+  });
+  it("has two toppings", function() {
+    var testPizza = new Pizza (2,"cheese","pepperoni");
+    expect(testPizza.price()).to.equal(11);
+  });
 });
